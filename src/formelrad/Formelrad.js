8cf785bae9457ef26010856355c454945d905f69
TAARUPA5 <panat.ruangsri@swisscom.com>
2025-06-15 23:51:32 +0200

    applied patches

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -11,6 +11,83 @@ export default function Formelrad() {
 		p: '',
 	});
 
+	const [colors, setColors] = useState({
+		u: 'black',
+		i: 'black',
+		r: 'black',
+		p: 'black',
+	});
+
+	const calculate = (event) => {
+		event.preventDefault();
+
+		const u = parseFloat(values.u);
+		const i = parseFloat(values.i);
+		const r = parseFloat(values.r);
+		const p = parseFloat(values.p);
+
+		if (isNaN(u)) {
+			console.warn('Spannung (u) muss gesetzt sein');
+			return;
+		}
+
+		if (values.i === '' && values.r === '') {
+			// calculate i and r
+			if (isNaN(p)) {
+				console.warn('Leistung (p) muss gesetzt sein für diese Berechnung');
+				return;
+			}
+			const newI = p / u;
+			const newR = (u * u) / p;
+			setValues((v) => ({
+				...v,
+				i: newI.toFixed(2),
+				r: newR.toFixed(2),
+			}));
+			setColors((c) => ({
+				...c,
+				i: 'black',
+				r: 'black',
+			}));
+		} else if (values.i === '' && values.p === '') {
+			// calculate i and p
+			if (isNaN(r)) {
+				console.warn('Widerstand (r) muss gesetzt sein für diese Berechnung');
+				return;
+			}
+			const newI = u / r;
+			const newP = (u * u) / r;
+			setValues((v) => ({
+				...v,
+				i: newI.toFixed(2),
+				p: newP.toFixed(2),
+			}));
+			setColors((c) => ({
+				...c,
+				i: 'red',
+				p: 'red',
+			}));
+		} else {
+			// calculate r and p
+			if (isNaN(i)) {
+				console.warn('Stromstärke (i) muss gesetzt sein für diese Berechnung');
+				return;
+			}
+			const newR = u / i;
+			const newP = u * i;
+			setValues((v) => ({
+				...v,
+				r: newR.toFixed(2),
+				p: newP.toFixed(2),
+			}));
+			setColors((c) => ({
+				...c,
+				r: 'blue',
+				p: 'blue',
+			}));
+		}
+	};
+
 	return (
 		<>
 			<section>
@@ -18,9 +95,9 @@ export default function Formelrad() {
 					<h2>Formelrad</h2>
 					<img src={formelrad} width="200" alt="Formelrad" />
 				</header>
-				<form>
+				<form onSubmit={calculate}>
 					<InputField
-						color={'black'}
+						color={colors.u}
 						value={values.u}
 						label="Spannung"
 						handleChange={(e) => {
@@ -28,7 +105,7 @@ export default function Formelrad() {
 						}}
 					/>
 					<InputField
-						color={'black'}
+						color={colors.i}
 						value={values.i}
 						label="Stromstärke"
 						handleChange={(e) => {
@@ -36,7 +113,7 @@ export default function Formelrad() {
 						}}
 					/>
 					<InputField
-						color={'black'}
+						color={colors.r}
 						value={values.r}
 						label="Widerstand"
 						handleChange={(e) => {
@@ -44,7 +121,7 @@ export default function Formelrad() {
 						}}
 					/>
 					<InputField
-						color={'black'}
+						color={colors.p}
 						value={values.p}
 						label="Leistung"
 						handleChange={(e) => {
